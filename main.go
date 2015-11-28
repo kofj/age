3f4694cc45a2f79d1be1ac28a443cc44ba065690
Frank Kung <kfanjian@gmail.com>
2015-11-28 13:50:15 +0800

    add main file.

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+var (
+	age      float64
+	birthday time.Time
+	err      error
+	fp       *os.File
+	input    string
+	now      time.Time
+
+	Bold = color.New(color.Bold).SprintFunc()
+	Cyan = color.New(color.FgCyan, color.Bold).SprintfFunc()
+)
+
+func main() {
+	getBirtday()
+	showAge()
+	fp.Close()
+}
+
+func getHomePath() string {
+	return os.Getenv("HOME")
+}
+
+func getBirtday() {
+	fp, err = os.OpenFile(getHomePath()+"/.age", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	defer fp.Close()
+	fstat, _ := fp.Stat()
+	if os.IsNotExist(err) || fstat.Size() != 10 {
+		setBirtday()
+	}
+	buf := make([]byte, 10)
+	fp.Read(buf)
+	birthday, _ = time.Parse("2006/01/02", string(buf))
+}
+
+func setBirtday() {
+	fmt.Printf("Please enter your birthday(format is %s),like %s\n", Bold("yyyy/MM/dd"), Bold("1991/03/04"))
+	fmt.Scanf("%s", &input)
+	birthday, err = time.Parse("2006/01/02", input)
+	if err != nil {
+		// Move the cursor to the origin, clean screen and show msg.
+		fmt.Println("\033[0;0H\033[2JError date format,try again.")
+		time.Sleep(5e8)
+		setBirtday()
+	}
+	fp.WriteString(input)
+	showAge()
+}
+
+func showAge() {
+	for {
+		now = time.Now()
+		age = float64(now.UnixNano()-birthday.UnixNano()) / 1e9 / 86400 / 365
+		fmt.Printf("Time flies, you've been %s years old.\r", Cyan("%3.9f", age))
+		time.Sleep(3e7)
+	}
+}
